8cb1bcd14132c49690ac81f9bb2bae06d6db3b3a
Sofmap <psm858196@gmail.com>
2022-03-24 17:54:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+function Header() {
+  return <header>
+  <h1><a href="/">React</a></h1>
+  </header>
+}
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/1">css</a></li>
+      <li><a href="/read/1">js</a></li>
+    </ol>
+  </nav>
+}
+function Article() {
+  return <article>
+  <h2>Welcom</h2>
+  Hello, WEB
+  </article>
+}
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
+}
+
+export default App;
